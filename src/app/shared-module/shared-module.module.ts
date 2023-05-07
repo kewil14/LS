64b1191ccfa743bf01f4fb32cd317cba545c5b60
landry <landryankam@gmail.com>
2023-05-07 12:37:28 +0100

    Date:07/05/2023(12h37)/implementation de radios

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -11,31 +11,34 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { StatComponent } from './widget/stat/stat.component';
 import { AddFormeComponent } from './components/formes/forms/add-forme/add-forme.component';
 import { AddCategoryComponent } from './components/categories/forms/add-category/add-category.component';
 import { AddFamilleComponent } from './components/familles/forms/add-famille/add-famille/add-famille.component';
 import { AddDciComponent } from './components/dci/forms/add-dci/add-dci.component';
-import { AddAntecedentValueComponent } from './components/antecedents-values/forms/add-antecedent-value/add-antecedent-value.component';
-import { AddTypeAntecedentComponent } from './components/types-antecedents/forms/add-type-antecedent/add-type-antecedent.component';
 import { AddTypeIntrantComponent } from './components/intrants/forms/add-type-intrant/add-type-intrant.component';
 import { AddIntrantValueComponent } from './components/intrants/forms/add-intrant-value/add-intrant-value.component';
 import { AddTypeTraitementComponent } from './components/traitments/forms/add-type-traitement/add-type-traitement.component';
 import { AddTraitementValueComponent } from './components/traitments/forms/add-traitement-value/add-traitement-value.component';
+import { AddRadioValueComponent } from './components/radios/forms/add-radio-value/add-radio-value.component';
+import { AddTypeRadioComponent } from './components/radios/forms/add-type-radio/add-type-radio.component';
+import { AddAntecedentValueComponent } from './components/antecedents/forms/add-antecedent-value/add-antecedent-value.component';
+import { AddTypeAntecedentComponent } from './components/antecedents/forms/add-type-antecedent/add-type-antecedent.component';
 
 
 @NgModule({
   declarations: [
-    LengthTextPipe, SuspensionPipe, RelativeDatePipe, StatComponent, AddFormeComponent, AddCategoryComponent,
+    LengthTextPipe, SuspensionPipe, RelativeDatePipe, AddFormeComponent, AddCategoryComponent,
     AddFamilleComponent, AddDciComponent, AddAntecedentValueComponent, AddTypeAntecedentComponent,
-    AddTypeIntrantComponent, AddIntrantValueComponent, AddTypeTraitementComponent, AddTraitementValueComponent
+    AddTypeIntrantComponent, AddIntrantValueComponent, AddTypeTraitementComponent, AddTraitementValueComponent,
+    AddRadioValueComponent, AddTypeRadioComponent
     
   ],
   exports: [
-    LengthTextPipe, SuspensionPipe,  RelativeDatePipe, StatComponent, AddFormeComponent, AddCategoryComponent,
-    LengthTextPipe, SuspensionPipe, RelativeDatePipe, StatComponent, AddFormeComponent, AddCategoryComponent, AddDciComponent, 
+    LengthTextPipe, SuspensionPipe,  RelativeDatePipe, AddFormeComponent, AddCategoryComponent,
+    LengthTextPipe, SuspensionPipe, RelativeDatePipe, AddFormeComponent, AddCategoryComponent, AddDciComponent, 
     AddAntecedentValueComponent, AddTypeAntecedentComponent, AddTraitementValueComponent, AddTypeTraitementComponent,
-    AddFamilleComponent, AddDciComponent, AddTypeIntrantComponent, AddIntrantValueComponent
+    AddFamilleComponent, AddDciComponent, AddTypeIntrantComponent, AddIntrantValueComponent, AddRadioValueComponent,
+    AddTypeRadioComponent
     
   ],
   imports: [
--- src/app/views/radios/radios-routing.module.ts
+++ src/app/views/radios/radios-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TypeRadioComponent } from './type-radio/type-radio.component';
 import { RadioValueComponent } from './radio-value/radio-value.component';
+import { LoadRadioValueGuard } from 'src/app/core/shared/guards/radios/load-radio-value.guard';
+import { LoadTypeRadioGuard } from 'src/app/core/shared/guards/radios/load-type-radio.guard';
 
 const routes: Routes = [
-  { path: 'types', component: TypeRadioComponent},
-  { path: 'values', component: RadioValueComponent},
+  { path: 'types', component: TypeRadioComponent, canActivate: [LoadTypeRadioGuard]},
+  { path: 'values', component: RadioValueComponent, canActivate: [LoadRadioValueGuard, LoadTypeRadioGuard]},
   { path: '**', redirectTo: 'values', pathMatch: 'full'},
 ];
 

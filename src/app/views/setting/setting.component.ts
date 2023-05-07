404b06c050c59c249f8c62d29947ff5e06de20db
kewil <kndjeyidjialeu@yahoo.com>
2023-05-07 14:11:26 +0200

    allergies

---
+++ src/app/views/setting/setting.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'health-setting',
+  templateUrl: './setting.component.html',
+  styleUrls: ['./setting.component.scss']
+})
+export class SettingComponent {
+
+}
--- src/app/views/views-routing.module.ts
+++ src/app/views/views-routing.module.ts
@@ -8,6 +8,9 @@ import { RadiosComponent } from './radios/radios.component';
 import { IntrantsComponent } from './intrants/intrants.component';
 import { TraitmentsComponent } from './traitments/traitments.component';
 import { LaboratoiresComponent } from './laboratoires/laboratoires.component';
+import { UserComponent } from './user/user.component';
+import { SettingComponent } from './setting/setting.component';
+import { InstitutionComponent } from './institution/institution.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
@@ -17,7 +20,10 @@ const routes: Routes = [
   { path: 'radios', component: RadiosComponent, loadChildren: () => import('./radios/radios.module').then(m => m.RadiosModule) },
   { path: 'intrants', component: IntrantsComponent, loadChildren: () => import('./intrants/intrants.module').then(m => m.IntrantsModule) },
   { path: 'traitments', component: TraitmentsComponent, loadChildren: () => import('./traitments/traitments.module').then(m => m.TraitmentsModule) },
-  { path: 'laboratoires', component: LaboratoiresComponent, loadChildren: () => import('./laboratoires/laboratoires.module').then(m => m.LaboratoiresModule) }
+  { path: 'laboratoires', component: LaboratoiresComponent, loadChildren: () => import('./laboratoires/laboratoires.module').then(m => m.LaboratoiresModule) },
+  { path: 'setting', component: SettingComponent, loadChildren: () => import('./setting/setting.module').then(m => m.SettingModule) },
+  { path: 'setting', component: UserComponent, loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: 'setting', component: InstitutionComponent, loadChildren: () => import('./institution/institution.module').then(m => m.InstitutionModule) },
 ];
 
 @NgModule({

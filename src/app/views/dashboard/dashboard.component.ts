2f23b82c4f27cb58ee5efd39600662612b5d73c9
landry <landryankam@gmail.com>
2023-05-07 16:20:19 +0100

    tmp yannk

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'health-dashboard',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+
+ 
 
 }
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { SimplebarAngularModule } from 'simplebar-angular';
 
 
 @NgModule({
@@ -11,7 +15,11 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    TabsModule.forRoot(),
+    NgApexchartsModule,
+    SimplebarAngularModule
+
   ]
 })
 export class DashboardModule { }

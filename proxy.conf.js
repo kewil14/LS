0a22539ea76a5c84906c2bfd2e2604ed8722d15c
singhor <cyankam@gmail.com>
2023-05-18 13:42:38 +0100

    2023-05-18

--- proxy.conf.js
+++ proxy.conf.js
@@ -5,6 +5,7 @@ const PROXY_CONFIG = [
       ],
       // target: 'http://78.46.11.10:8777',
       target: 'http://161.97.80.60:8777',
+      // target: 'https://gateway.health-care.cm',
       secure: false
   }
 ]
